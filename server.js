79bfb8bb2a3ebe140c534426c8d67481e17cfa63
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-18 15:17:21 +0100

    Add 404 error handling middleware to return JSON response for not found errors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const postsRouter = require('./routers/posts'); 
 const serverError = require('./middlewares/server_error')
+const error_404 = require('./middlewares/error_404')
 
 app.listen(port, () => {
     console.log('Server is listening on http://localhost:' + port);
@@ -19,3 +20,6 @@ app.use(serverError)
 
 
 app.use('/posts', postsRouter);
+
+
+app.use(error_404)
